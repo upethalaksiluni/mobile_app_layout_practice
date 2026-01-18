9b9e90ca1d8ca3897d436bd1805304926d2e8440
Upetha Laksiluni <upethalaksiluni@gmail.com>
2026-01-18 16:18:09 +0530

    Style changed

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,24 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class MyFirstWidget extends StatelessWidget {
+  const MyFirstWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("MyFirstWidget"));
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("MyWidget"));
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
@@ -86,144 +104,166 @@ class _MyHomePageState extends State<MyHomePage> {
           // the App.build method, and use it to set our appbar title.
           title: Center( child: Text(widget.title, textAlign: TextAlign.center,),),
         ),
-        body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Container(
-            child: Column(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top: 34),
-                  child: Text(
-                    'Welcome to Flutter!',
-                    style: TextStyle(
-                      fontSize: 22,
+
+        body: Container(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,  //for the space between welcome to flutter and hello flutter
+              ),
+              Container(
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      gradient: LinearGradient(colors: [Colors.red, Colors.amber]),
+                      // borderRadius: BorderRadius.all(Radius.elliptical(5, 25)),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black26,
+                            offset: Offset(0 , 5),
+                          blurRadius: 9
+
+                        )
+                      ],
+
+                  ),
+                  height: 40,
+                  width: 380,
+                  child: Center(
+                    child: Text('Welcome to Flutter!', style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
                       fontWeight: FontWeight.bold,
-                    ),
+                      fontStyle: FontStyle.italic,
+                      decoration: TextDecoration.lineThrough,
+                      decorationColor: Colors.red,
+                      decorationThickness: 2,
+                      decorationStyle: TextDecorationStyle.dashed,
+                      overflow: TextOverflow.fade,
+                    ),),
+                  )
+              ),
+              SizedBox(
+                height: 20,  //for the space between welcome to flutter and hello flutter
+              ),
+
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.red, Colors.amber],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
                   ),
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      offset: Offset(0, 5),
+                      blurRadius: 9,
+                    ),
+                  ],
                 ),
-      
-                Container(
-                  height: 45,
-                  width: 500,
-                  margin: EdgeInsets.only(top: 34),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.lightGreen,
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.zero,
-                      )
-                    ),
-                    onPressed: () {},
-                    child: const Text(
-                      'Hello, Flutter!',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
+                height: 40,
+                width: 380,
+                child: Center(
+                  child: Text('Hello Flutter!'),
                 ),
-                Column(
-                  children: [Row(
-                    children: [
-                      Container(
-                        width: 137,
-                        height: 70,
-                        margin: EdgeInsets.only(top: 24),
-                        color: Colors.red,
-                        child: Center(child: Text('Box 1')),
-                      ),
-                      Container(
-                        width: 137,
-                        height: 70,
-                        margin: EdgeInsets.only(top: 24),
-                        color: Colors.green,
-                        child: Center(child: Text('Box 2')),
-                      ),
-                      Container(
-                        width: 137,
-                        height: 70,
-                        margin: EdgeInsets.only(top: 24),
-                        color: Colors.blue,
-                        child: Center(child: Text('Box 3')),
-                      ),
-                    ],
-                  ),
-      
-      
-                    Container(
-                      margin: EdgeInsets.only(top: 25),
-                      child: Text('First Widget',
-                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black54)),
-                    ),
-      
-                    Container(
-                      height: 45,
-                      width: 500,
-                      margin: EdgeInsets.only(top: 34),
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.lightGreen,
-                            shape: const RoundedRectangleBorder(
-                              borderRadius: BorderRadius.zero,
-                            )
-                        ),
-                        onPressed: () {},
-                        child: const Text(
-                          'Second Widget',
-                          style: TextStyle(color: Colors.white),
+              ),
+
+              SizedBox(
+                height: 20,  //for the space between welcome to flutter and hello flutter
+              ),
+              Container(
+                width: 380,
+                child: Row(
+                  children: [
+                    Expanded(flex: 2,
+                      //   use to set width in empty space
+                      child: Container(
+                        height: 50,
+                        color: Colors.deepOrange,
+                        child: Center(
+                            child: Text('Item 1')
                         ),
                       ),
                     ),
-      
-                  ]
-      
+                    SizedBox(
+                      width: 10,  //for the space between welcome to flutter and hello flutter
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 50,
+
+                        color: Colors.deepPurple,
+                        child: Center(
+                            child: Text('Item 2')
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,  //for the space between welcome to flutter and hello flutter
+                    ),
+                    Expanded(child:
+                    Container(
+                      height: 50,
+
+                      color: Colors.red,
+                      child: Center(
+                          child: Text('Item 3')
+                      ),
+                    ),)
+                  ],
                 ),
-      
-                Container(
-                  height: 45,
-                  width: 500,
-                  margin: EdgeInsets.only(top: 34),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.lightGreen,
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.zero,
-                        )
-                    ),
-                    onPressed: () {},
-                    child: const Text(
-                      'Third Widgetss',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-      
-                Spacer(),
-                Container(
-                  height: 45,
-                  width: 500,
-                  margin: EdgeInsets.only(top: 34),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.amber,
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.zero,
-                        )
-                    ),
-                    onPressed: () {},
-                    child: const Text(
-                      'Footer text',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-      
+              ),
+
+              SizedBox(
+                height: 20,
+              ),
+              MyFirstWidget(),
+
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                  color: Colors.brown,
+                  height: 40,
+                  width: 380,
+                  child: Center(
+                    child: Text('First Widget'),
+                  )
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                  color: Colors.greenAccent,
+                  height: 40,width: 380,
+                  child: Center(
+                    child: Text('Second Widget'),
+                  )
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                  color: Colors.cyanAccent,
+                  height: 40,
+                  width: 380,
+                  child: Center(
+                    child: Text('Third Widget'),
+                  )
+              ),
+              Spacer(),
+              Container(
+                  color: Colors.black12,
+                  height: 50,
+                  child: Center(
+                    child: Text('Footer'),
+                  )
+              ),
+            ],
           ),
-      
-      
         ),
-      
       ),
     );
   }
